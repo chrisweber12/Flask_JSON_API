d4ede7dfedb71ca9e233b9291d9df11e12d29dca
chrisweber12 <65516703+chrisweber12@users.noreply.github.com>
2022-02-03 16:12:20 -0500

    Add program files via upload

---
+++ __init__.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+with open('data.json') as file:
+    data = json.load(file)
+
+def recipeNames():
+    return [recipe['name'] for recipe in data['recipes']]
+
+@app.route('/')
+def index():
+    return 'Welcome to the recipe page'
+
+@app.route('/recipes')
+def get_recipes():
+    return jsonify({'recipeNames': recipeNames()})
+
+@app.route('/recipes', methods=['POST'])
+def add_recipe():
+    if request.get_json()['name'] in recipeNames():
+        return jsonify({'error':'Recipe already exists'}), 400
+    else:
+        data['recipes'].append(request.get_json())
+        return '', 201
+
+@app.route('/recipes', methods=['PUT'])
+def alter_recipe():
+    if request.get_json()['name'] not in recipeNames():
+        return jsonify({'error':'Recipe does not exist'}), 404
+    else:
+        for i, recipe in enumerate(data['recipes']):
+            if recipe['name'] == request.get_json()['name']:
+                data['recipes'][i] = request.get_json()
+        return '', 204
+
+@app.route('/recipes/details/<recipeName>')
+def details(recipeName):
+    for recipe in data['recipes']:
+        if recipe['name'] == recipeName:
+            return jsonify({'details':{'ingredients':recipe['ingredients'],'numSteps':len(recipe['instructions'])}})
+    return jsonify({})
---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=__init__.py
+source venv/bin/activate
+flask run -h localhost -p 3000
